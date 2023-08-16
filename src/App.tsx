237a8ce9f5b9b8e606ddb6122190de0a39e73b58
Nabeel Anjum <timedilation2012@gmail.com>
2023-08-17 04:38:57 +0500

    make: cart store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { View } from "react-native";
+import { Provider } from "react-redux";
 
 import RootNavigation from "./navigation";
+import store from "./store";
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <RootNavigation />
+      <Provider store={store}>
+        <RootNavigation />
+      </Provider>
     </View>
   );
 }
--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Pressable, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import Home from '../screens/Home';
+import Cart from '../screens/Cart';
 
 const AppStack = () => {
   const Stack = createStackNavigator();
 
+  const navigation = useNavigation();
+
   return (
     <Stack.Navigator>
       {/* Normally would keep the whole app's routes in a separate file */}
-      <Stack.Screen name={"Products"} component={Home} options={{ title: "Products" }} />
+      <Stack.Screen
+        name={"Products"}
+        component={Home}
+        options={{
+          title: "Products",
+          headerRight: () => (
+            <Pressable style={{ marginHorizontal: 20 }} onPress={() => navigation.navigate("Cart")}>
+              <Text>Cart</Text>
+            </Pressable>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name={"Cart"}
+        component={Cart}
+      />
     </Stack.Navigator>
   );
 }
